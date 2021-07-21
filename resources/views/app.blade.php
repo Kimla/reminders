4f90834f34881caace52ce48af8c0699b4303f22
Kim Larsson <kim@bravissimo.se>
2021-07-21 22:14:36 +0200

    Added favicon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,6 +7,8 @@
 
         <title>{{ config('app.name', 'Laravel') }}</title>
 
+        <link rel="shortcut icon" type="image/png" href="/favicon.png">
+
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
