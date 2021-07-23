15c39fa79b69b4c3de06664ad2d712a5a9be63ce
Kim Larsson <kim@bravissimo.se>
2021-07-23 20:45:21 +0200

    Fixed path

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -11,7 +11,7 @@ window.axios = require('axios');
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 if('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('sw.js');
+    navigator.serviceWorker.register('/sw.js');
 };
 
 /**
